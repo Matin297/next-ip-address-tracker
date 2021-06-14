b0d7beea21c8b99dc8af3f65f18e496f1f9cf4e0
asma mir <asmamir297@gmail.com>
2021-06-14 08:44:29 +0430

    Removes the marker from utils

--- components/button/arrow.js
+++ components/button/arrow.js
@@ -4,7 +4,7 @@ export default function Arrow(props) {
     return (
         <button className={classes.arrowBtn} {...props}>
             <svg xmlns="http://www.w3.org/2000/svg" width="11" height="14">
-                <path fill="none" stroke="#FFF" stroke-width="3" d="M2 1l6 6-6 6" />
+                <path fill="none" stroke="#FFF" strokeWidth="3" d="M2 1l6 6-6 6" />
             </svg>
         </button>
     )
--- components/map.js
+++ components/map.js
@@ -1,10 +1,23 @@
 import { useRef, useEffect } from 'react'
+import L from 'leaflet';
 import { MapContainer, TileLayer, Marker } from 'react-leaflet';
 import { URL, ATTRIBUTE } from '../utils/mapbox';
-import { markerIcon } from '../utils/marker';
 import "leaflet/dist/leaflet.css";
 import "leaflet-defaulticon-compatibility/dist/leaflet-defaulticon-compatibility.css";
 import "leaflet-defaulticon-compatibility";
+// ICONS
+const MarkerSVG = '/images/icon-location.svg';
+
+const markerIcon = new L.Icon({
+    iconUrl: MarkerSVG,
+    iconRetinaUrl: MarkerSVG,
+    iconAnchor: null,
+    popupAnchor: null,
+    shadowUrl: null,
+    shadowSize: null,
+    shadowAnchor: null,
+    iconSize: new L.Point(60, 75)
+});
 
 export default function Map({ pos }) {
     const mapRef = useRef();
